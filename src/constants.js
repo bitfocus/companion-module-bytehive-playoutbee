f8c62643c374b4a43fa773e2ddd9275323c4621c
Joseph Adams <josephdadams@gmail.com>
2022-09-02 08:18:44 -0400

    add support for 0.9.4

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,6 @@ module.exports = {
 	initConstants() {
 		this.defineConst('DEFAULT_IP', '127.0.0.1')
 		this.defineConst('DEFAULT_PORT', '3000')
-		this.defineConst('COMPATIBLE_VERSIONS', ['0.9.3'])
+		this.defineConst('COMPATIBLE_VERSIONS', ['0.9.3', '0.9.4'])
 	},
 }
